0e620e48d29f7c692e96a4cd079f2285635b3068
Myypo <nikirsmcgl@gmail.com>
2022-11-17 13:43:05 +0200

    Refactored message client

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,72 +3,20 @@ package main
 import (
 	"bufio"
 	"context"
-	"crypto/sha256"
-	"encoding/hex"
-	"flag"
-	"fmt"
 	"log"
 	"os"
-	"sync"
-	"time"
 
 	"google.golang.org/grpc/credentials/insecure"
 
 	client_service "github.com/MyyPo/grpc-chat/cmd/client/service"
-	chatpb "github.com/MyyPo/grpc-chat/pb/chat/v1"
 	"google.golang.org/grpc"
-	"google.golang.org/protobuf/types/known/timestamppb"
 )
-
-var chatClient chatpb.BroadcastServiceClient
-var wait *sync.WaitGroup
-
-func init() {
-	wait = &sync.WaitGroup{}
-}
-
-func connect(user *chatpb.User) error {
-	var streamerr error
-
-	stream, err := chatClient.ServerMessageStream(context.Background(), &chatpb.ServerMessageStreamRequest{
-		User:   user,
-		Active: true,
-	})
-
-	if err != nil {
-		return fmt.Errorf("failed to connect: %v", err)
-	}
-
-	wait.Add(1)
-	go func(stream chatpb.BroadcastService_ServerMessageStreamClient) {
-		defer wait.Done()
-
-		for {
-			msg, err := stream.Recv()
-
-			if err != nil {
-				streamerr = fmt.Errorf("failed to read a message: %v", err)
-				break
-			}
-
-			fmt.Printf("%v : %s\n", msg.Id, msg.Content)
-		}
-	}(stream)
-
-	return streamerr
-}
 
 var scanner *bufio.Scanner
 
 func main() {
-	timestamp := time.Now()
 
-	done := make(chan int)
-
-	name := flag.String("N", "Anon", "User name")
-	flag.Parse()
-
-	id := sha256.Sum256([]byte(timestamp.String() + *name))
+	scanner = bufio.NewScanner(os.Stdin)
 
 	conn, err := grpc.Dial("localhost:8080", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
@@ -76,42 +24,13 @@ func main() {
 	}
 	scanner = bufio.NewScanner(os.Stdin)
 
-	AuthClient := client_service.NewSignInClient(conn, scanner)
+	authClient := client_service.NewSignInClient(conn, scanner)
 
-	AuthClient.SignIn(context.Background())
+	authClient.SignIn(context.Background())
 
-	chatClient = chatpb.NewBroadcastServiceClient(conn)
-	user := &chatpb.User{
-		Id:   hex.EncodeToString(id[:]),
-		Name: *name,
-	}
+	chatClient := client_service.NewChatClient(conn, scanner)
 
-	connect(user)
+	chatClient.ServerMessageStream(context.Background())
+	chatClient.ClientMessage(context.Background())
 
-	wait.Add(1)
-	go func() {
-		defer wait.Done()
-
-		scanner := bufio.NewScanner(os.Stdin)
-		for scanner.Scan() {
-			msg := &chatpb.Message{
-				Id:        user.Id,
-				Content:   scanner.Text(),
-				Timestamp: timestamppb.New(time.Now()),
-			}
-
-			_, err := chatClient.ClientMessage(context.Background(), msg)
-			if err != nil {
-				fmt.Printf("Error while sending a message: %v", err)
-				break
-			}
-		}
-	}()
-
-	go func() {
-		wait.Wait()
-		close(done)
-	}()
-
-	<-done
 }
---
+++ cmd/client/service/chat.go
@@ -0,0 +1,109 @@
+package client_service
+
+import (
+	"bufio"
+	"context"
+	"crypto/sha256"
+	"encoding/hex"
+	// "flag"
+	"fmt"
+	"sync"
+	"time"
+
+	chatpb "github.com/MyyPo/grpc-chat/pb/chat/v1"
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+type ChatClient struct {
+	service chatpb.BroadcastServiceClient
+	scanner *bufio.Scanner
+	wait    *sync.WaitGroup
+}
+
+func NewChatClient(conn *grpc.ClientConn, scanner *bufio.Scanner) *ChatClient {
+	service := chatpb.NewBroadcastServiceClient(conn)
+	wait := &sync.WaitGroup{}
+
+	return &ChatClient{service, scanner, wait}
+}
+
+func (client *ChatClient) ServerMessageStream(ctx context.Context) error {
+	var streamerr error
+
+	timestamp := timestamppb.Now()
+	// name := flag.String("N", "Anon", "User name")
+	// flag.Parse()
+	id := sha256.Sum256([]byte(timestamp.String() + "Name"))
+	user := &chatpb.User{
+		Id:   hex.EncodeToString(id[:]),
+		Name: "Name",
+	}
+
+	stream, err := client.service.ServerMessageStream(context.Background(), &chatpb.ServerMessageStreamRequest{
+		User:   user,
+		Active: true,
+	})
+
+	if err != nil {
+		return fmt.Errorf("failed to connect: %v", err)
+	}
+
+	client.wait.Add(1)
+	go func(stream chatpb.BroadcastService_ServerMessageStreamClient) {
+		defer client.wait.Done()
+
+		for {
+			msg, err := stream.Recv()
+
+			if err != nil {
+				streamerr = fmt.Errorf("failed to read a message: %v", err)
+				break
+			}
+
+			fmt.Printf("%v : %s\n", msg.Id, msg.Content)
+		}
+	}(stream)
+
+	return streamerr
+}
+
+func (client *ChatClient) ClientMessage(ctx context.Context) {
+	timestamp := timestamppb.Now()
+	// name := flag.Parse()
+	// name := flag.String("N", "Anon", "User name")
+	id := sha256.Sum256([]byte(timestamp.String() + "Name"))
+
+	user := &chatpb.User{
+		Id:   hex.EncodeToString(id[:]),
+		Name: "Name",
+	}
+
+	done := make(chan int)
+
+	client.wait.Add(1)
+	go func() {
+		defer client.wait.Done()
+
+		for client.scanner.Scan() {
+			msg := &chatpb.Message{
+				Id:        user.Id,
+				Content:   client.scanner.Text(),
+				Timestamp: timestamppb.New(time.Now()),
+			}
+
+			_, err := client.service.ClientMessage(context.Background(), msg)
+			if err != nil {
+				fmt.Printf("Error while sending a message: %v", err)
+				break
+			}
+		}
+	}()
+
+	go func() {
+		client.wait.Wait()
+		close(done)
+	}()
+
+	<-done
+}
